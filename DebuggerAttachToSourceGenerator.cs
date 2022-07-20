1d69ee2c467a00c080c8fc29bd168837b3c3fb15
Sebastien Pouliot <sebastien.pouliot@gmail.com>
2022-07-20 09:33:40 -0400

    Initial import

---
+++ DebuggerAttachToSourceGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+
+namespace MyGenerator
+{
+	[Generator]
+	public class DebuggerAttachToSourceGenerator : ISourceGenerator
+	{
+		public void Execute(GeneratorExecutionContext context)
+		{
+		}
+
+		public void Initialize(GeneratorInitializationContext context)
+		{
+			Console.Error.WriteLine($"Debug Source Generator Initializing... Attach to PID: {Process.GetCurrentProcess().Id}");
+			while (!Debugger.IsAttached)
+			{
+				Thread.Sleep(100);
+			}
+			Debugger.Break();
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # debugsourcegen
-Helper to debug source generators
+
+Helper to debug source generators.
+
+This is useful when you cannot change the source code of existing generators.
+You simply add the following line to yur `.csproj` file, making it the first
+analyzer.
+
+```xml
+/analyzer:/Users/poupou/git/debugsourcegen/bin/Debug/netstandard2.0/debugsourcegen.dll \
+```
+
+This will print the port where the CoreCLR debugger (e.g. from VSfM) can attach
+and will next break the execution to let you add additional breakpoints.
